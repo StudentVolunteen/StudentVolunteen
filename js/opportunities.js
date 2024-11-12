4ac787c1062c75b62515ecf2e546d9427e0fd8ce
vedsinghh <vedsingh1208@gmail.com>
2024-11-11 17:33:49 -0800

    sdjfb

--- js/opportunities.js
+++ js/opportunities.js
@@ -1,27 +1,21 @@
+const accountEmail = document.querySelector('.account-email');
 // Import the functions you need from the SDKs you need
-import {initializeApp} from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDvsS06wEMIc7WW30WxKfmu8R-4xKLJ6Ag",
-    authDomain: "volunteen-438f6.firebaseapp.com",
-    projectId: "volunteen-438f6",
-    storageBucket: "volunteen-438f6.appspot.com",
-    messagingSenderId: "651971950022",
-    appId: "1:651971950022:web:8709d63dbcbb745125eec7"
+  apiKey: "AIzaSyDvsS06wEMIc7WW30WxKfmu8R-4xKLJ6Ag",
+  authDomain: "volunteen-438f6.firebaseapp.com",
+  projectId: "volunteen-438f6",
+  storageBucket: "volunteen-438f6.appspot.com",
+  messagingSenderId: "651971950022",
+  appId: "1:651971950022:web:8709d63dbcbb745125eec7"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
-function writeUserData(userId, name, email) {
-  app.database().ref('users').set(ref(db, 'users/' + userId), {
-  username: name,
-  email: email
-  });
-}
-
-writeUserData(1, "tester", "test@gmail.com");
   
