be12a1cfad0f038ebeee6746be86577630425e9f
vedsinghh <vedsingh1208@gmail.com>
2024-11-11 17:12:55 -0800

    progress

--- js/opportunities.js
+++ js/opportunities.js
@@ -1,3 +1,9 @@
+// Import the functions you need from the SDKs you need
+import {initializeApp} from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyDvsS06wEMIc7WW30WxKfmu8R-4xKLJ6Ag",
     authDomain: "volunteen-438f6.firebaseapp.com",
@@ -5,5 +11,17 @@ const firebaseConfig = {
     storageBucket: "volunteen-438f6.appspot.com",
     messagingSenderId: "651971950022",
     appId: "1:651971950022:web:8709d63dbcbb745125eec7"
-  };
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+function writeUserData(userId, name, email) {
+  app.database().ref('users').set(ref(db, 'users/' + userId), {
+  username: name,
+  email: email
+  });
+}
+
+writeUserData(1, "tester", "test@gmail.com");
   
