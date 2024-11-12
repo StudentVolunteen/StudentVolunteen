cdfd16192158e103e4a21c1b7822f78db7e31d51
DemarcusCousins2 <67602812+DemarcusCousins2@users.noreply.github.com>
2024-11-11 16:53:02 -0800

    new commit

---
+++ js/opportunities.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDvsS06wEMIc7WW30WxKfmu8R-4xKLJ6Ag",
+    authDomain: "volunteen-438f6.firebaseapp.com",
+    projectId: "volunteen-438f6",
+    storageBucket: "volunteen-438f6.appspot.com",
+    messagingSenderId: "651971950022",
+    appId: "1:651971950022:web:8709d63dbcbb745125eec7"
+  };
+  
